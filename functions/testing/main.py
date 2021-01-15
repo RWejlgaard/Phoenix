b1b88d8bea74a372db83ea28687540758388e381
Rasmus Wejlgaard <pez@pez.sh>
2021-01-15 23:23:17 +0000

    added testing function

---
+++ functions/testing/main.py
@@ -0,0 +1,19 @@
+from urllib.request import Request, urlopen
+import os
+import json
+
+
+def main(request):
+    try:
+        data = request.get_json()
+    except AttributeError:
+        data = request
+
+    ret = ""
+
+    print(ret)
+    return ret
+
+
+if __name__ == '__main__':
+    main({})
