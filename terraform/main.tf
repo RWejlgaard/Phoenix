8d40c8029e803e754335c77e6df3f2c35781b9ee
Rasmus Wejlgaard <pez@pez.sh>
2021-01-15 23:23:04 +0000

    added api-endpoint service for pez.link (swagger)

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,3 +9,8 @@ module "lambda-function" {
   staging_bucket_name = google_storage_bucket.bucket.name
   openweathermap-api-key = var.openweathermap-api-key
 }
+
+resource "google_endpoints_service" "openapi_service" {
+  service_name   = "phoenix.apigateway.rwejlgaard.cloud.goog"
+  openapi_config = file("./files/phoenix-api.yaml")
+}
